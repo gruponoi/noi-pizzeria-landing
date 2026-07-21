b973c1e1c423d3fae582d0ebb2d89f214c154031
Trosky Pucha <troskytops@gmail.com>
2026-07-20 22:39:05 -0500

    chore: respaldo inicial del proyecto

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <h1>NOI PIZZERÍA</h1>
+
+      <p>Porque Loja se lo merece 🍕</p>
+
+      <div className="buttons">
+        <button>Ver Menú</button>
+        <button>Pedir por WhatsApp</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
